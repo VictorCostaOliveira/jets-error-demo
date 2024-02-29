b87bd728ec43081f7e90c561d6d81279d64b9d82
Victor Costa <victorcosta@jera.com.br>
2024-02-29 16:49:51 +0100

    Firsr commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Jets.application.routes.draw do
+  resources :posts
   root "jets/welcome#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
